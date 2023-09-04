af9d1a785c3fba6fb2d458f937e56e099ce4677e
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-04 18:00:14 -0300

    Created prefab
    Created walkable tile prefab

--- UltimateTowerDefense/Assets/Scrips/Grid/Tiles/CoordinateLabeler.cs
+++ UltimateTowerDefense/Assets/Scrips/Grid/Tiles/CoordinateLabeler.cs
@@ -12,23 +12,32 @@ public class CoordinateLabeler : MonoBehaviour
 
     private Vector2Int coordinates = new Vector2Int();
     private TextMeshPro labelText;
+    private Tile tile;
 
     private void Awake()
     {   
         labelText = GetComponent<TextMeshPro>();
+        tile = GetComponentInParent<Tile>();
     }
 
     private void Update()
-    {
-        if (!Application.isPlaying)
+    {  
+        if (Input.GetKeyDown(KeyCode.U))
         {
-            DisplayCoordinates();
+            ToggleCordinateLabeler();
         }
+
+        DisplayCoordinates();
+    }
+
+    private void ToggleCordinateLabeler()
+    {
+        labelText.enabled = !labelText.enabled;
     }
 
     private void DisplayCoordinates()
     {
-        if (!labelText)
+        if (!labelText.enabled)
         {
             return;
         }
--- UltimateTowerDefense/Assets/Scrips/Grid/Tiles/Tile.cs
+++ UltimateTowerDefense/Assets/Scrips/Grid/Tiles/Tile.cs
@@ -1,16 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileStatus { Free, Walkable, Occuped}
 
 public class Tile : MonoBehaviour
 {
     [Header("Tile")]
     [SerializeField] private int tileSize = 5;
+    [SerializeField] private TileStatus tileStatus = TileStatus.Free;
+
 
     private Vector2Int tileCoordinates = new Vector2Int();
 
+
+    public Vector2Int TileCoordinates { get { return tileCoordinates; } }
+
+    private void Awake()
+    {
+        SetTileCoordinates();
+    }    
+
     private void Update()
+    {
+        
+    }
+
+    private void SetTileCoordinates()
     {
         tileCoordinates.x = Mathf.RoundToInt(transform.position.x / tileSize);
         tileCoordinates.y = Mathf.RoundToInt(transform.position.z / tileSize);
