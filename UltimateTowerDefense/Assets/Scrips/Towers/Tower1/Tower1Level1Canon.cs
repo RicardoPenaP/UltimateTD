b29f5191718d3558e3e389c49debc1b59dea4576
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-05 15:23:09 -0300

    Created class
    Created Tower 1 Level 1 Canon

--- UltimateTowerDefense/Assets/Scrips/Towers/Tower1/Tower1Level1Canon.cs
+++ UltimateTowerDefense/Assets/Scrips/Towers/Tower1/Tower1Level1Canon.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tower1Level1Canon : MonoBehaviour
+public class Tower1Level1Canon : MonoBehaviour,ITowerCanon
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
