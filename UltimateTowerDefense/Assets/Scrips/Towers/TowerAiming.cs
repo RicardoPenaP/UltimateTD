03c4d332382855365fc14a3725589bbd5b85edcf
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-05 15:52:32 -0300

    Implemented feature
    Implemeted Tower Aiming class Update Tower Canon method

--- UltimateTowerDefense/Assets/Scrips/Towers/TowerAiming.cs
+++ UltimateTowerDefense/Assets/Scrips/Towers/TowerAiming.cs
@@ -4,10 +4,87 @@ using UnityEngine;
 
 public class TowerAiming : MonoBehaviour
 {
+    [Header("Tower Aiming")]   
+
     private ITowerCanon myCanon;
+    private TowerController myController;
+    private EnemyController target;
+
+    private bool canAttack = true;
+    private float attackCooldownTime;
 
     private void Awake()
     {
         myCanon = GetComponentInChildren<ITowerCanon>();
+        myController = GetComponent<TowerController>();
+    }
+
+    private void Start()
+    {
+        SetAttackCooldownTime(myController.AttackRatio);
+    }
+
+    private void Update()
+    {
+        UpdateTowerCanon();
+    }
+
+    private void SetAttackCooldownTime(float attackRatio)
+    {
+        attackCooldownTime = 1 / attackRatio;
+    }
+
+    private void UpdateTowerCanon()
+    {
+        SetTarget();
+
+        if (!target)
+        {
+            return;
+        }
+
+        AimTowerCanon();
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (!canAttack)
+        {
+            return;
+        }
+        canAttack = false;
+        myCanon.Attack();
+        StartCoroutine(AttackCooldownTime());
+    }
+
+    private void AimTowerCanon()
+    {
+        myCanon.AimAt(target.transform.position);
+    }
+
+    private void SetTarget()
+    {
+        //Takes all the near objects and search for a valid enemy in range
+        Collider[] nearObjects = Physics.OverlapSphere(transform.position, myController.AttackRange);       
+
+        foreach (Collider nearObject in nearObjects)
+        {
+            EnemyController enemy = nearObject.GetComponent<EnemyController>();
+            if (enemy)
+            {
+                target = enemy;
+                return;
+            }
+        }
+
+        //If doesn't find a valid target set the target null
+        target = null;
+    }
+
+    private IEnumerator AttackCooldownTime()
+    {
+        yield return new WaitForSeconds(attackCooldownTime);
+        canAttack = true;
     }
 }
--- UltimateTowerDefense/Assets/Scrips/Towers/TowerController.cs
+++ UltimateTowerDefense/Assets/Scrips/Towers/TowerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TowerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Tower Controller")]
+    [Tooltip("Amount of damage dealt per attack")]
+    [SerializeField, Min(0)] private int attackDamage = 100;
+    [Tooltip("Amount of attacks per second")]
+    [SerializeField, Min(0)] private float attackRatio = 1;
+    [Tooltip("The range for dectecting enemies and attack them")]
+    [SerializeField, Min(0f)] private float attackRange = 10f;
 
-    // Update is called once per frame
-    void Update()
+    public int AttackDamage { get { return attackDamage; } }
+    public float AttackRatio { get { return attackRatio; } }
+    public float AttackRange { get { return attackRange; } }
+
+    //Debugging Tools
+    private void OnDrawGizmosSelected()
     {
-        
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
