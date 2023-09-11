eb8ef1c4edf402d5f5ef7d5c189d475290d511ba
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-11 19:37:46 -0300

    Implemeted feature
    Implemented UI Bar class functionalities

--- UltimateTowerDefense/Assets/Scrips/UI/UIBar.cs
+++ UltimateTowerDefense/Assets/Scrips/UI/UIBar.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("UIBar")]
+    private TextMeshProUGUI barText;
+    private Image fillImage;
+
+    private void Awake()
     {
-        
+        barText = GetComponentInChildren<TextMeshProUGUI>();
+        fillImage = transform.GetChild(1).GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateBar(int currentValue, int maxValue)
     {
-        
+        barText.text = $"{currentValue}/{maxValue}";
+        fillImage.fillAmount = (float)currentValue / (float)maxValue;
+    }
+
+    public void UpdateBar(float currentValue, float maxValue)
+    {
+        barText.text = $"{currentValue}/{maxValue}";
+        fillImage.fillAmount = currentValue / maxValue;
     }
 }
