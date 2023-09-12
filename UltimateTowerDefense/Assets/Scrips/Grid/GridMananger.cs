352eb42dd330b28f830fcbef7977f26992b89949
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-12 17:40:23 -0300

    Implemented feature
    Implemeted max and min position values in the grid for tresholds references

--- UltimateTowerDefense/Assets/Scrips/Grid/GridMananger.cs
+++ UltimateTowerDefense/Assets/Scrips/Grid/GridMananger.cs
@@ -2,19 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GridMananger : Singleton<GridMananger>
 {
+    public struct MapTresholds
+    {
+        public float minX, maxX, minZ, maxZ;
+    }
+
     [Header("Grid Mananger")]
     public static readonly int tileSize = 5;    
 
     private Dictionary<Vector2Int, Tile> mapGrid = new Dictionary<Vector2Int, Tile>();
-   
+
+    private MapTresholds myTresholds = new MapTresholds();
+
     public Dictionary<Vector2Int, Tile> MapGrid { get { return mapGrid; } }
+    public MapTresholds MyThresholds { get { return myTresholds; } }
 
     protected override void Awake()
     {
         base.Awake();
         UpdateTiles();
+        SetTresholds();
+    }
+
+    private void SetTresholds()
+    {
+        float minX=0, maxX=0, minZ=0, maxZ=0;
+        foreach (KeyValuePair<Vector2Int,Tile> tile in mapGrid)
+        {
+            if (tile.Value.transform.position.x < minX)
+            {
+                minX = tile.Value.transform.position.x;
+            }
+
+            if (tile.Value.transform.position.x > maxX)
+            {
+                maxX = tile.Value.transform.position.x;
+            }
+
+            if (tile.Value.transform.position.z < minZ)
+            {
+                minZ = tile.Value.transform.position.z;
+            }
+
+            if (tile.Value.transform.position.z > maxZ)
+            {
+                maxZ = tile.Value.transform.position.z;
+            }
+        }
+
+        myTresholds.minX = minX;
+        myTresholds.maxX = maxX;
+        myTresholds.minZ = minZ;
+        myTresholds.maxZ = maxZ;
     }
 
     public void UpdateTiles()
