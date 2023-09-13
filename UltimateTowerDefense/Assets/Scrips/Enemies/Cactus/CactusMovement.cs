e1fee7799f214fa12bbb7b9a1149406c3d0c8374
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-13 19:26:58 -0300

    Implemented feature
    Implemented Cactus Movement class functionalities

--- UltimateTowerDefense/Assets/Scrips/Enemies/Cactus/CactusMovement.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/Cactus/CactusMovement.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class CactusMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private EnemyController myController;
+
+    private Vector3 positionToMove;
+
+    private bool positionReached = true;
+    public bool PositionReached { get { return positionReached; } }
+
+    private void Awake()
     {
-        
+        myController = GetComponentInParent<EnemyController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!myController.IsAlive)
+        {
+            return;
+        }
+
+        if (!myController.CanWalk)
+        {
+            return;
+        }
+
+        if (positionReached)
+        {
+            return;
+        }
+
+        transform.position += transform.forward * myController.MovementSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(positionToMove, transform.position) <= myController.DistanceFromNextTileOffset)
+        {
+            transform.position = positionToMove;
+            positionReached = true;
+        }
+    }
+
+    private void SetMovementDirection()
+    {
+        transform.forward = (positionToMove - transform.position).normalized;
+    }
+
+    public void SetPositionToMove(Vector3 positionToMove)
+    {
+        this.positionToMove = positionToMove;
+        positionReached = false;
+        SetMovementDirection();
     }
 }
