da8a0cb196c65d3ec7ec7798603fc9cf85ce1a3d
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-13 20:01:37 -0300

    Implemented feature
    Implemented Golem Animator Helper class functionalities

--- UltimateTowerDefense/Assets/Scrips/Enemies/Golem/GolemAnimatorHelper.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/Golem/GolemAnimatorHelper.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class GolemAnimatorHelper : MonoBehaviour
 {
-   
+    private GolemIA myIA;
+
+    private void Awake()
+    {
+        myIA = GetComponentInParent<GolemIA>();
+    }
+
+    public void AttackAnimationCompleted()
+    {
+        myIA.AttackAnimationCompleted();
+    }
+
+    public void DieAnimationCompleted()
+    {
+        myIA.DieAnimationCompleted();
+    }
 }
