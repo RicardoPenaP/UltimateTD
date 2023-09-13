c6313cb38842e778a4c1510af3c2f52a087fb91f
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-13 20:00:56 -0300

    Implemented feature
    Implemented Golem Movement class functionalities

--- UltimateTowerDefense/Assets/Scrips/Enemies/Golem/GolemMovement.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/Golem/GolemMovement.cs
@@ -4,5 +4,58 @@ using UnityEngine;
 
 public class GolemMovement : MonoBehaviour
 {
-    
+    private EnemyController myController;
+
+    private Vector3 positionToMove;
+
+    private bool positionReached = true;
+    public bool PositionReached { get { return positionReached; } }
+
+    private void Awake()
+    {
+        myController = GetComponentInParent<EnemyController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!myController.IsAlive)
+        {
+            return;
+        }
+
+        if (!myController.CanWalk)
+        {
+            return;
+        }
+
+        if (positionReached)
+        {
+            return;
+        }
+
+        transform.position += transform.forward * myController.MovementSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(positionToMove, transform.position) <= myController.DistanceFromNextTileOffset)
+        {
+            transform.position = positionToMove;
+            positionReached = true;
+        }
+    }
+
+    private void SetMovementDirection()
+    {
+        transform.forward = (positionToMove - transform.position).normalized;
+    }
+
+    public void SetPositionToMove(Vector3 positionToMove)
+    {
+        this.positionToMove = positionToMove;
+        positionReached = false;
+        SetMovementDirection();
+    }
 }
