5b5aee51f761c021ab6e0cba5d8ffd2c0d8f7a7d
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-14 14:09:19 -0300

    Implemeted feature
    Implemented Enemy Data class functionalities

--- UltimateTowerDefense/Assets/Scrips/Enemies/EnemyData.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/EnemyData.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyData : MonoBehaviour
+[CreateAssetMenu(fileName ="NewEnemyData",menuName ="EnemyData")]
+public class EnemyData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Enemy Data")]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Reference Settings")]
+    [SerializeField] private EnemyController enemyPrefab;
+
+    [Header("Health Settings")]
+    [SerializeField, Min(1f)] private int maxHealth = 100;
+    [SerializeField, Min(0f)] private int maxShield = 100;
+
+    [Header("Level Settings")]
+    [SerializeField, Min(1f)] private float healthLevelMultiplier = 1.5f;
+
+    [Header("Movement Settings")]
+    [SerializeField, Min(0f)] private float movementSpeed;   
+    [Tooltip("The minimun near distance from the next tile to cosider that you are in there")]
+    [SerializeField, Min(0f)] private float distanceFromNextTileOffset = 0.08f;
+
+    [Header("Rewards Settings")]
+    [SerializeField, Min(0f)] private int goldReward = 25;
+
+    [Header("Damage Settings")]
+    [SerializeField] private int damageToStronghold = 1;
+
+
+    public EnemyController EnemyPrefab { get { return enemyPrefab; } }
+    public int MaxHealth { get { return maxHealth; } }
+    public int MaxShield { get { return maxShield; } }
+    public float HealthLevelMultiplier { get { return healthLevelMultiplier; } }
+    public float MovementSpeed { get { return movementSpeed; } }
+    public float DistanceFromNextileOffset { get { return distanceFromNextTileOffset; } }
+    public int GoldReward { get { return goldReward; } }
+    public int DamageToStronghold { get { return damageToStronghold; } }
+
+
 }
