92a0f702cd22ec34884a32f4ac21674edae38211
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-14 15:04:13 -0300

    Implemented feature
    Implemented Enemy UI Follower class functionalities

--- UltimateTowerDefense/Assets/Scrips/Enemies/EnemyUI/EnemyUIFollower.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/EnemyUI/EnemyUIFollower.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EnemyUIFollower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private EnemyDamageHandler myDamageHandler;
+
+    private void Awake()
     {
-        
+        myDamageHandler = GetComponent<EnemyDamageHandler>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateUIPos();
+    }
+
+    private void UpdateUIPos()
+    {
+        transform.position = new Vector3(myDamageHandler.transform.position.x, transform.position.y, myDamageHandler.transform.position.z);
     }
 }
