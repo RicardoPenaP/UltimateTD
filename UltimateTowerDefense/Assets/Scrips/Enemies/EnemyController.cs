db248144bcdbeb6366b34d5fd26d09d0264c256a
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-14 14:18:12 -0300

    Implemented feature
    Implemented Enemy Data class functionalities and members on Enemy Controller class

--- UltimateTowerDefense/Assets/Scrips/Enemies/EnemyController.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/EnemyController.cs
@@ -6,33 +6,36 @@ using EnemiesInterface;
 public class EnemyController : MonoBehaviour
 {
     [Header("Enemy Controller")]
+    [SerializeField] private EnemyData myData;
 
-    [Header("Health Settings")]
-    [SerializeField] private int maxHealth = 100;
+    //[Header("Health Settings")]
+    //[SerializeField] private int maxHealth = 100;
 
-    [Header("Movement Settings")]
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private bool canWalk;
-    [SerializeField] private float distanceFromNextTileOffset = 0.05f;
+    //[Header("Movement Settings")]
+    //[SerializeField] private float movementSpeed;
+   
+    //[SerializeField] private float distanceFromNextTileOffset = 0.05f;
 
-    [Header("Rewards Settings")]
-    [SerializeField,Min(0f)] private int goldReward = 25;
+    //[Header("Rewards Settings")]
+    //[SerializeField,Min(0f)] private int goldReward = 25;
 
-    [Header("Damage Settings")]
-    [SerializeField] private int damageToStronghold = 1;
+    //[Header("Damage Settings")]
+    //[SerializeField] private int damageToStronghold = 1;
 
-    //private EnemyMovement myMovement;
+    
     private IEnemy myEnemyIA;
 
-    [SerializeField]private int currentHealth;
+    private int currentHealth;
+    private int currentShield;
 
     private bool isAlive = true;
-    
-    public float MovementSpeed { get { return movementSpeed; } }
+    private bool canWalk;
+
+    public float MovementSpeed { get { return myData.MovementSpeed; } }
     public bool CanWalk { get { return canWalk; } }
-    public float DistanceFromNextTileOffset { get { return distanceFromNextTileOffset; } }
-    public int GoldReward { get { return goldReward; } }
-    public int DamageToStronghold { get { return damageToStronghold; } }
+    public float DistanceFromNextTileOffset { get { return myData.DistanceFromNextileOffset; } }
+    public int GoldReward { get { return myData.GoldReward; } }
+    public int DamageToStronghold { get { return myData.DamageToStronghold; } }
     public bool IsAlive { get { return isAlive; } }
 
     private void Awake()
@@ -50,14 +53,10 @@ public class EnemyController : MonoBehaviour
         transform.localPosition = Vector3.zero; 
     }
 
-    private void Start()
-    {
-        currentHealth = maxHealth;
-    }
-
     private void ResetEnemy()
     {
-        currentHealth = maxHealth;
+        currentHealth = myData.MaxHealth;
+        currentShield = myData.MaxShield;
         canWalk = true;
         isAlive = true;
     }
@@ -73,7 +72,25 @@ public class EnemyController : MonoBehaviour
         {
             return;
         }
-        currentHealth -= damageAmount;
+
+        if (currentShield > 0)
+        {
+            if (currentShield >= damageAmount)
+            {
+                currentShield -= damageAmount;
+            }
+            else
+            {
+                int leftDamage = damageAmount - currentShield;
+                currentShield = 0;
+                currentHealth -= leftDamage;
+            }
+        }
+        else
+        {
+            currentHealth -= damageAmount;
+        }
+        
         if (currentHealth <= 0)
         {
             isAlive = false;
