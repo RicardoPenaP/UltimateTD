9ec6ccf07615f1634a277aba9504624f2e8269c5
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-19 15:05:59 -0300

    Improved feature
    Improved Wave Data class members information with a tooltip description

--- UltimateTowerDefense/Assets/Scrips/Manangers/WaveMananger/WaveData.cs
+++ UltimateTowerDefense/Assets/Scrips/Manangers/WaveMananger/WaveData.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 [System.Serializable]
 public struct EnemyToSpawn
 {
+    [Header("Enemy To Spawn")]
+    [Tooltip("Prefab reference")]
     [SerializeField] private EnemyData enemyPrefab;
+    [Tooltip("Amount of total units of this enemy to spawn along the wave")]
     [SerializeField, Min(0)] private int amountToSpawn;
+    [Tooltip("Amount of time between each unit spawn along the wave")]
     [SerializeField, Min(0f)] private float timeBetweenSpawn;
+    [Tooltip("The unit level")]
     [SerializeField, Range(1,100)] private int enemyLevel;
+    [Tooltip("If this unit is a Boss or not")]
+    [SerializeField] private bool isABoss;
 
     public EnemyData EnemyPrefab { get { return enemyPrefab; } }
     public int AmountToSpawn { get { return amountToSpawn; } }
     public float TimeBetweenSpawn { get { return timeBetweenSpawn; } }
     public int EnemyLevel { get { return enemyLevel; } }
+    public bool IsABoss { get { return isABoss; } }
 }
 
 [CreateAssetMenu(fileName = "NewWaveData",menuName ="WaveData")]
