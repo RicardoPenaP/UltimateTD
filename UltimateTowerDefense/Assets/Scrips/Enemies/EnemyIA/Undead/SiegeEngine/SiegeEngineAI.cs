b8ef771cd69347b64b4b4254080c24eb6d43c8e8
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-27 14:40:05 -0300

    Implemented feature
    Implemented Siege Engine Ammo movement

--- UltimateTowerDefense/Assets/Scrips/Enemies/EnemyIA/Undead/SiegeEngine/SiegeEngineAI.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/EnemyIA/Undead/SiegeEngine/SiegeEngineAI.cs
@@ -1,18 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EnemiesInterface;
 
 public class SiegeEngineAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Siege Engine AI")]
+    [SerializeField] private SiegeEngineAmmo ammoPrefab;
+    [SerializeField] private Transform shootPosition;
+
+    private readonly int ATTACK_HASH = Animator.StringToHash("Attack");
+
+    private EnemyController myController;
+    private EnemyAnimatorHelper myAnimatorHelper;
+    private EnemyMovement myMovement;
+    private Animator myAnimator;
+
+    private EnemyState myState;
+
+    private bool canAttack = true;
+
+    private void Awake()
     {
-        
+        myController = GetComponent<EnemyController>();
+        myAnimatorHelper = GetComponentInChildren<EnemyAnimatorHelper>();
+        myMovement = GetComponent<EnemyMovement>();
+        myAnimator = GetComponentInChildren<Animator>();
+        InitEventsSubscriptions();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        myState = EnemyState.Walking;
+        canAttack = true;
+    }
+
+    private void Update()
+    {
+        UpdateState();
+    }
+
+    private void InitEventsSubscriptions()
+    {
+        SetAttackAnimationsEvents();
+        myMovement.OnPathEnded += PathEnded;
+    }
+
+    private void SetAttackAnimationsEvents()
+    {
+        myAnimatorHelper.OnAttackAnimationStarted += () => { canAttack = false; };
+        myAnimatorHelper.OnAttackAnimationPerformed += () => { HealthMananger.Instance.TakeDamage(myController.DamageToStronghold); Shoot(); };
+        myAnimatorHelper.OnAttackAnimationEnded += () => { canAttack = true; };
+    }
+
+    private void UpdateState()
+    {
+        switch (myState)
+        {
+            case EnemyState.None:
+                break;
+            case EnemyState.Walking:
+                Walking();
+                break;
+            case EnemyState.Attacking:
+                Attacking();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Walking()
+    {
+        if (Vector3.Distance(transform.position, HealthMananger.Instance.GetStrongholdPos()) <= myController.AttackRange)
+        {
+            PathEnded();
+        }
+    }
+
+    private void Attacking()
+    {
+        if (!canAttack)
+        {
+            return;
+        }
+        Attack();
+    }
+
+    private void Attack()
+    {
+        myAnimator.SetTrigger(ATTACK_HASH);
+    }
+
+    private void PathEnded()
+    {
+        myController.SetCanMove(false);
+        myState = EnemyState.Attacking;
+    }
+
+    private void Shoot()
+    {
+        Instantiate(ammoPrefab, shootPosition.position, myAnimator.transform.rotation, transform).GetComponent<SiegeEngineAmmo>().SetRange(myController.AttackRange);
     }
 }
--- UltimateTowerDefense/Assets/Scrips/Enemies/EnemyIA/Undead/SiegeEngine/SiegeEngineAmmo.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/EnemyIA/Undead/SiegeEngine/SiegeEngineAmmo.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class SiegeEngineAmmo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Siege Engine Ammo")]
+    [SerializeField, Min(1f)] private float timeOfTravel = 1f;
+    [Tooltip("Is the percentage of the distance that will translate to the max height of the curve")]
+    [SerializeField, Range(0f, 100f)] private float maxHeightPercentage = 50;
+    [Tooltip("Is the representaive curve off the movement")]
+    [SerializeField] private AnimationCurve heightCurve;
+
+    private float range;
+    private Vector3 startPos;
+    private Vector3 finalPos;
+    private float maxY;
+
+    public void SetRange(float range)
     {
-        
+        this.range = range;
+        startPos = transform.position;
+        finalPos = startPos + (transform.forward * range);
+        maxY = (Vector3.Distance(startPos, finalPos) * maxHeightPercentage) / 100;
+        StartCoroutine(ParableMovementRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroyBehaviour()
     {
-        
+        //Destroy Behaviour
+        gameObject.SetActive(false);
+        Destroy(gameObject);
     }
+
+    private IEnumerator ParableMovementRoutine()
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < timeOfTravel)
+        {
+            elapsedTime += Time.deltaTime;
+            Vector3 newPos = new Vector3();
+            float deltaT = elapsedTime / timeOfTravel;
+            float heightT = heightCurve.Evaluate(deltaT);
+            newPos.x = Mathf.Lerp(startPos.x, finalPos.x, deltaT);
+            newPos.y = Mathf.Lerp(startPos.y, maxY, heightT);
+            newPos.z = Mathf.Lerp(startPos.z, finalPos.z, deltaT);
+
+            transform.position = newPos;
+            yield return null;
+        }
+
+        DestroyBehaviour();
+    }
+
 }
