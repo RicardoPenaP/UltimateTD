38c523a9cc571ebb26c17875623c98c0fd853fb1
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-27 15:02:21 -0300

    Implemented feature
    Implemented Base Melee AI functionalities

--- UltimateTowerDefense/Assets/Scrips/Enemies/EnemyIA/BaseMeleeAI.cs
+++ UltimateTowerDefense/Assets/Scrips/Enemies/EnemyIA/BaseMeleeAI.cs
@@ -1,8 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EnemiesInterface;
 
 public class BaseMeleeAI : MonoBehaviour
 {
-    
+    [Header("Base Melee AI")]
+    private readonly int ATTACK_HASH = Animator.StringToHash("Attack");
+
+    private EnemyController myController;
+    private EnemyAnimatorHelper myAnimatorHelper;
+    private EnemyMovement myMovement;
+    private Animator myAnimator;
+
+    private EnemyState myState;
+
+    private bool canAttack = true;
+
+    private void Awake()
+    {
+        myController = GetComponent<EnemyController>();
+        myAnimatorHelper = GetComponentInChildren<EnemyAnimatorHelper>();
+        myMovement = GetComponent<EnemyMovement>();
+        myAnimator = GetComponentInChildren<Animator>();
+        InitEventsSubscriptions();
+    }
+
+    private void OnEnable()
+    {
+        ResetEnemy();
+    }
+
+    private void Update()
+    {
+        UpdateState();
+    }
+
+    private void ResetEnemy()
+    {
+        myState = EnemyState.Walking;
+        canAttack = true;
+    }
+
+    private void InitEventsSubscriptions()
+    {
+        SetAttackAnimationsEvents();
+        myMovement.OnPathEnded += PathEnded;
+    }
+
+    private void SetAttackAnimationsEvents()
+    {
+        myAnimatorHelper.OnAttackAnimationStarted += () => { canAttack = false; };
+        myAnimatorHelper.OnAttackAnimationPerformed += () => { HealthMananger.Instance.TakeDamage(myController.DamageToStronghold); };
+        myAnimatorHelper.OnAttackAnimationEnded += () => { canAttack = true; };
+    }
+
+    private void UpdateState()
+    {
+        switch (myState)
+        {
+            case EnemyState.Walking:
+                Walking();
+                break;
+            case EnemyState.Attacking:
+                Attacking();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Walking()
+    {
+        if (Vector3.Distance(transform.position, HealthMananger.Instance.GetStrongholdPos()) <= myController.AttackRange)
+        {
+            PathEnded();
+        }
+    }
+
+    private void Attacking()
+    {
+        if (!canAttack)
+        {
+            return;
+        }
+        Attack();
+    }
+
+    private void Attack()
+    {
+        myAnimator.SetTrigger(ATTACK_HASH);
+    }
+
+    private void PathEnded()
+    {
+        myController.SetCanMove(false);
+        myState = EnemyState.Attacking;
+    }
+
 }
