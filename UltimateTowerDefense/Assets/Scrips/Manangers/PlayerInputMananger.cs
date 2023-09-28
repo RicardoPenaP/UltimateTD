2b886c0566d31ef06742bccc2e277145b6ed9a18
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-28 18:09:19 -0300

    Created class
    Created Player Input Mananger

--- UltimateTowerDefense/Assets/Scrips/Manangers/PlayerInputMananger.cs
+++ UltimateTowerDefense/Assets/Scrips/Manangers/PlayerInputMananger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneManagement : MonoBehaviour
+public class PlayerInputMananger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
