6661f0563586303e95fa15f058febd6a7bd8312a
RicardoPenaP <ricardopena241197@gmail.com>
2023-09-29 13:05:20 -0300

    Implemented feature
    Implemented button displacement on Game Over Menu

--- UltimateTowerDefense/Assets/Scrips/Menus/GameOverMenu/GameOverMenu.cs
+++ UltimateTowerDefense/Assets/Scrips/Menus/GameOverMenu/GameOverMenu.cs
@@ -20,16 +20,31 @@ public class GameOverMenu : MonoBehaviour
 
     private void SubmitToButtonsEvents()
     {
+        mainMenuButton?.onClick.AddListener(GoToMainMenu);
+        playAgainButton?.onClick.AddListener(PlayAgain);
+    }
 
+    private void GoToMainMenu()
+    {
+        gameObject.SetActive(false);
+        GameScenesLoader.LoadGameScene(GameScenes.MainMenu);
+    }
+
+    private void PlayAgain()
+    {
+        gameObject.SetActive(false);
+        GameScenesLoader.ReloadCurrentScene();
     }
 
     public void OpenGameOverMenu()
     {
         titleText.text = "Game Over";
+        gameObject.SetActive(true);
     }
 
     public void OpenGameCompleteMenu()
     {
         titleText.text = "Congratulations, you successfully defended your kingdom";
+        gameObject.SetActive(true);
     }
 }
