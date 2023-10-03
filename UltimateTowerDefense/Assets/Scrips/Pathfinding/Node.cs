85bb1ed161f3012f5fc1994f3441109203b3f9d8
RicardoPenaP <ricardopena241197@gmail.com>
2023-10-03 14:11:40 -0300

    Reorganized assets

--- UltimateTowerDefense/Assets/Scrips/Pathfinding/Node.cs
+++ UltimateTowerDefense/Assets/Scrips/Pathfinding/Node.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
+
+public enum NodeContent { Grass, Water, Path, Stronghold,Decoration}
+public class Node 
 {
     public Vector2Int coordinates;
-    public bool isWalkable;
-    public bool isExplored;
-    public bool isPath;
-    public Node connectedTo;
-
-    public Node(Vector2Int coordinates, bool isWalkable)
-    {
-        this.coordinates = coordinates;
-        this.isWalkable = isWalkable;
-    }
+    public Vector3 position;
+    public NodeContent content;
+   
 }
