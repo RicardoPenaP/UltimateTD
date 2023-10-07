52f6619857e11beca20b2c52c69b33ee7bda6fec
RicardoPenaP <ricardopena241197@gmail.com>
2023-10-06 21:14:15 -0300

    Created class
    Created Game Data class

---
+++ UltimateTowerDefense/Assets/Scrips/Manangers/GameMananger/GameData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : ScriptableObject
+{
+    //[Header("Game Data")]
+    //[Header("Enemies Prefabs")]
+    //[Header("Undeads")]
+    //[SerializeField] private EnemyController
+
+}
