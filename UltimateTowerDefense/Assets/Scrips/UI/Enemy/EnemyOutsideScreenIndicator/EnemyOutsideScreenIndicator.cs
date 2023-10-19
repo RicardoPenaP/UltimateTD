dbb4adb57077da70dff443c37cd3c75a03c9235a
RicardoPenaP <ricardopena241197@gmail.com>
2023-10-19 19:34:13 -0300

    Created class
    Created EnemyOutsideScreenIndicator class

---
+++ UltimateTowerDefense/Assets/Scrips/UI/Enemy/EnemyOutsideScreenIndicator/EnemyOutsideScreenIndicator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOutsideScreenIndicator : MonoBehaviour
+{
+    [Header("Enemy OutsideScreenIndicator")]
+    [SerializeField] private GameObject indicatorPrefab;
+
+    
+}
