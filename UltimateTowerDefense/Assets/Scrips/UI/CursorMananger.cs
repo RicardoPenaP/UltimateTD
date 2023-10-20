d20dd1d6944159d211ea1da6edff0699f0ead511
RicardoPenaP <ricardopena241197@gmail.com>
2023-10-20 17:08:21 -0300

    Added assets package

--- UltimateTowerDefense/Assets/Scrips/UI/CursorMananger.cs
+++ UltimateTowerDefense/Assets/Scrips/UI/CursorMananger.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorMananger : MonoBehaviour
+public class CursorMananger : Singleton<CursorMananger>
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Awake()
     {
-        
+        base.Awake();
+        if (CursorMananger.Instance == this)
+        {
+            DontDestroyOnLoad(transform.parent.gameObject);
+        }        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        Cursor.visible = false;
+        if (Application.isPlaying)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 cursorPos = Input.mousePosition;
+        transform.position = cursorPos;
         
+        Cursor.visible = !MouseIsIntoTheViewport();
+
+
+    }
+
+    private bool MouseIsIntoTheViewport()
+    {
+        return (Input.mousePosition.x >= 0 && Input.mousePosition.x <= Screen.width) && (Input.mousePosition.y >= 0 && Input.mousePosition.y <= Screen.height);
     }
 }
